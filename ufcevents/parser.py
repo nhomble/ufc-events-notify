e8ecbe68666f70bb3cae61a6127fd0204249fb5d
nhomble <nhomble@terpmail.umd.edu>
2021-12-14 11:30:29 -0500

    print parsed

--- ufcevents/parser.py
+++ ufcevents/parser.py
@@ -9,13 +9,15 @@ def _get_cards(soup):
     divs = soup.find_all("div", class_="c-card-event--result__info")
 
     def _parse(div):
-        return {
+        ret = {
             "title": div.find("h3").text,
             "date_full": div.find("div")["data-main-card"],
             "date_day": div.find("div")["data-main-card"].split("/")[0].strip(),
             "link": _url + div.find("a")['href'],
             "parsed_date": datetime.now().strftime("%m-%d-%Y")
         }
+        print(f"Parsed div={ret}")
+        return ret
 
     return [_parse(div) for div in divs]
 
